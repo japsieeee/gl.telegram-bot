63c3604dc8d0a30233fa92fe9290da52ed2dd01f
japsieeee <dyollsopaj@gmail.com>
2025-01-28 18:13:54 +0900

    chore: update

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,9 +7,9 @@ const child_process_1 = require("child_process");
 const promises_1 = require("fs/promises");
 const path_1 = __importDefault(require("path"));
 class TelegramBot {
+    BOT_TOKEN = '';
+    SCRIPT_DIR = path_1.default.join(__dirname, 'scripts');
     constructor(botToken) {
-        this.BOT_TOKEN = '';
-        this.SCRIPT_DIR = path_1.default.join(__dirname, 'scripts');
         this.BOT_TOKEN = botToken;
     }
     /**
@@ -47,14 +38,14 @@ class TelegramBot {
      */
     getUpdates() {
         return new Promise((res) => {
-            (0, child_process_1.exec)(`bash ${this.SCRIPT_DIR}telegram-get-updates.sh "${this.BOT_TOKEN}"`, (err, stdout) => __awaiter(this, void 0, void 0, function* () {
+            (0, child_process_1.exec)(`bash ${this.SCRIPT_DIR}telegram-get-updates.sh "${this.BOT_TOKEN}"`, async (err, stdout) => {
                 if (!err) {
                     // console.log('stdout', stdout);
-                    const result = yield (0, promises_1.readFile)(`get_updates_${this.BOT_TOKEN}.json`);
+                    const result = await (0, promises_1.readFile)(`get_updates_${this.BOT_TOKEN}.json`);
                     const jsonResult = JSON.parse(result.toString());
                     res(jsonResult);
                 }
-            }));
+            });
         });
     }
     /**
@@ -64,22 +55,21 @@ class TelegramBot {
      */
     getGroupIDs() {
         return new Promise((res) => {
-            (0, child_process_1.exec)(`bash ${this.SCRIPT_DIR}telegram-get-updates.sh "${this.BOT_TOKEN}"`, (err, stdout) => __awaiter(this, void 0, void 0, function* () {
+            (0, child_process_1.exec)(`bash ${this.SCRIPT_DIR}telegram-get-updates.sh "${this.BOT_TOKEN}"`, async (err, stdout) => {
                 if (!err) {
                     // console.log('stdout', stdout);
-                    const result = yield (0, promises_1.readFile)(`get_updates_${this.BOT_TOKEN}.json`);
+                    const result = await (0, promises_1.readFile)(`get_updates_${this.BOT_TOKEN}.json`);
                     const jsonResult = JSON.parse(result.toString());
                     const idSet = new Set([]);
                     jsonResult.result.forEach((v) => {
-                        var _a;
-                        const chatID = (_a = v.message) === null || _a === void 0 ? void 0 : _a.chat.id;
+                        const chatID = v.message?.chat.id;
                         if (chatID && !idSet.has(chatID))
                             idSet.add(chatID);
                     });
                     const arrayOfID = [...idSet];
                     res(arrayOfID);
                 }
-            }));
+            });
         });
     }
 }
--- dist/sample.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_js_1 = __importDefault(require("./index.js"));
-const botInstance = new index_js_1.default('7452405493:AAFEXa-potfzmUyBAgRGvqnXJq-ZM88ajrU');
-// botInstance.sendMessage('sample again', '-1002309719445')
-botInstance.sendMessage('sample again', '-1002373158292');
