eb8d438a446620602765df3e9f6b362511548f2d
japsieeee <dyollsopaj@gmail.com>
2025-01-28 17:58:59 +0900

    chore: update

---
+++ dist/sample.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const _1 = __importDefault(require("."));
+const botInstance = new _1.default('7452405493:AAFEXa-potfzmUyBAgRGvqnXJq-ZM88ajrU');
+// botInstance.sendMessage('sample again', '-1002309719445')
+botInstance.sendMessage('sample again', '-1002373158292');
