52175dc5ed023099a521f531b672a6a59e3fffb8
japsieeee <dyollsopaj@gmail.com>
2025-01-28 17:59:34 +0900

    chore: update

--- dist/sample.js
+++ dist/sample.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = __importDefault(require("."));
-const botInstance = new _1.default('7452405493:AAFEXa-potfzmUyBAgRGvqnXJq-ZM88ajrU');
+const index_js_1 = __importDefault(require("./index.js"));
+const botInstance = new index_js_1.default('7452405493:AAFEXa-potfzmUyBAgRGvqnXJq-ZM88ajrU');
 // botInstance.sendMessage('sample again', '-1002309719445')
 botInstance.sendMessage('sample again', '-1002373158292');
